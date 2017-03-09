a2ae46d705f6843445174ca5e85434d2e318a1d3
Rick Fletcher <fletch@pobox.com>
2017-03-09 09:55:01 -0500

    Support multiple versions via java::version

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,19 +5,20 @@ class java(
   include ::java::params
 
   validate_re( $distribution, '^(jre|jdk)$' )
-  validate_re( $version, '^[0-9]?$' )
 
-  if $version =~ /^[0-9]$/ {
-    $major_version = $version
+  if is_array( $version ) {
+    $distribution_classes = suffix( prefix( $version, '::java::' ), "::${distribution}" )
   } else {
-    $major_version = $::java::params::java['default']
+    validate_re( "${version}", '^[0-9]?$' ) # must cast int to string
+
+    if $version =~ /^[0-9]$/ {
+      $major_version = $version
+    } else {
+      $major_version = $::java::params::java['default']
+    }
+
+    $distribution_classes = ["::java::${major_version}::${distribution}"]
   }
 
-  $distribution_class = "::java::${major_version}::${distribution}"
-
-  if ! defined( $distribution_class ) {
-    fail( "Java distribution \"${distribution}\" is not supported for version ${major_version}." )
-  } else {
-    include $distribution_class
-  }
+  include $distribution_classes
 }
